60b8e30e5162c13a783ed31868dbbd10f0c8fcee
BrianLipscombe <brianjohnlipscombe@yahoo.com>
2022-02-11 19:22:47 +0000

    Update date range

--- static/js/script.js
+++ static/js/script.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
     $("select").formSelect();
     $(".datepicker").datepicker({
         format: "dd mmmm, yyyy",
-        yearRange: 3,
+        yearRange: 4,
         showClearBtn: true,
         i18n: {
             done: "Select"
